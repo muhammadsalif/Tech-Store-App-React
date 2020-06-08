bef9515b4f725550e337823c15894bb7322b8b2a
Muhammad Salif <muhammad.salif@yahoo.com>
2020-06-08 14:56:28 +0500

    Context API methods done and state setup done.

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -3,9 +3,52 @@ import React, { Component } from "react";
 const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
+  state = {
+    sidebarOpen: false,
+    cartOpen: false,
+    cartItems: 0,
+  };
+
+  // Handle side bar
+  handleSidebar = () => {
+    this.setState({
+      sidebarOpen: !this.state.sidebarOpen,
+    });
+  };
+
+  // Handle side cart
+  handleCart = () => {
+    this.setState({
+      cartOpen: !this.state.cartOpen,
+    });
+  };
+
+  // Close cart
+  closeCart = () => {
+    this.setState({
+      cartOpen: false,
+    });
+  };
+
+  // Close cart
+  OpenCart = () => {
+    this.setState({
+      cartOpen: true,
+    });
+  };
+
   render() {
     return (
-      <ProductContext.Provider value="Hello from the context API">
+      <ProductContext.Provider
+        value={{
+          // Using spread operator for passing all state values
+          ...this.state,
+          handleSidebar: this.handleSidebar,
+          handleCart: this.handleCart,
+          closeCart: this.closeCart,
+          OpenCart: this.OpenCart,
+        }}
+      >
         {this.props.children}
       </ProductContext.Provider>
     );
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import { ProductConsumer } from "../context";
 
 function HomePage() {
-  return <ProductConsumer>{(value) => <h1>{value}</h1>}</ProductConsumer>;
+  return (
+    <ProductConsumer>
+      {(value) => {
+        console.log(value);
+        return <h1>Hello from home page</h1>;
+      }}
+    </ProductConsumer>
+  );
 }
 
 export default HomePage;

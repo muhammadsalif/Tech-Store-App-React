7cc7c3054458a6121d8fe4875d4091efc8a30430
Muhammad Salif <muhammad.salif@yahoo.com>
2020-06-09 17:16:51 +0500

    Contact page done with formspree setup.

--- src/components/ContactPage/Contact.jsx
+++ src/components/ContactPage/Contact.jsx
@@ -8,7 +8,11 @@ function Contact() {
         <div className="col-10 mx-auto col-md-6 my-3 ">
           <Title title="contact us"></Title>
 
-          <form className="my-5">
+          <form
+            className="my-5"
+            action="https://formspree.io/xjvarvoo"
+            method="POST"
+          >
             {/* first */}
             <div className="form-group">
               <input
@@ -38,13 +42,21 @@ function Contact() {
               />
             </div>
             {/* text area */}
-            <div className="form">
+            <div className="form-group">
               <textarea
                 name="message"
                 className="form-control"
                 rows="10"
                 placeholder="hello there buddy"
               ></textarea>
+            </div>
+            {/* submit */}
+            <div className="form-group mt-3">
+              <input
+                type="submit"
+                className="form-control bg-primary text-white text-capitalize"
+                value="submit"
+              />
             </div>
           </form>
         </div>
